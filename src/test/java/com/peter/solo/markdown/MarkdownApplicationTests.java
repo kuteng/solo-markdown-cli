41192d816a3e0dcf2f319bff63f004f179fbf072
yandong.great <yandong.great@bytedance.com>
2020-01-29 16:06:06 +0800

    使用Spring Initializr初始化该项目
    Change-Id: Ic4cfc36107fedc13cd17a479c9a8dc9ff978fa25

---
+++ src/test/java/com/peter/solo/markdown/MarkdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.solo.markdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
