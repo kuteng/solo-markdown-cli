275bed2c70dc1f50bddd8d1123e8e33be68befbb
yandong.great <yandong.great@bytedance.com>
2020-08-26 19:12:08 +0800

    备份h2的配置
    Change-Id: Ic04c595cd6bc68232e1a6ebbaf37b572ebc3cede

---
+++ src/main/resources/db/data-h2.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO user (account_id, name, token) VALUES
+--   ('1', 'Dangote', 'BillionaireIndustrialist'),
+--   ('2', 'Gates', 'BillionaireTechEntrepreneur'),
+--   ('3', 'Alakija', 'BillionaireOilMagnate');
+delete from USER where ID = -1;
